60dc124abe8633c3c853e73ce47a4edc31d05e51
IverNando <hcarrassets@gmail.com>
2026-02-10 13:45:18 +0000

    fix: multiple heads

---
+++ migrations/versions/22c26a39466a_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 22c26a39466a
+Revises: 21c1f61f813a, b9094827b701
+Create Date: 2026-02-10 13:41:49.870676
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '22c26a39466a'
+down_revision = ('21c1f61f813a', 'b9094827b701')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
