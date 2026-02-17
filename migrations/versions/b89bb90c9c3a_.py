5797bb583e552bb6cb298c484ffc7d6a57672980
IverNando <hcarrassets@gmail.com>
2026-02-17 00:10:57 +0000

    ahora si, CRUD completo de appointments

--- migrations/versions/b89bb90c9c3a_.py
+++ migrations/versions/b89bb90c9c3a_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 10115da49242
+Revision ID: b89bb90c9c3a
 Revises: 
-Create Date: 2026-02-16 23:45:40.624513
+Create Date: 2026-02-16 23:58:05.416728
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '10115da49242'
+revision = 'b89bb90c9c3a'
 down_revision = None
 branch_labels = None
 depends_on = None
